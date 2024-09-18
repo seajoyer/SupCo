8e524d4ceb1960272ac8132dc0288090e7b7b734
seajoyer <imgarison@gmail.com>
2024-09-18 22:10:52 +0300

    flake

--- flake.nix
+++ flake.nix
@@ -4,39 +4,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-
-    # Referencing subdirectories on GitHub
-    argument_search.url = "github:seajoyer/SupCo?dir=Argument_search";
-    machine_units.url = "github:seajoyer/SupCo?dir=Machine_units";
+    machine-units.url = "path:./Machine_units";
+    argument-search.url = "path:./Argument_search";
   };
 
-  outputs = { self, nixpkgs, flake-utils, argument_search, machine_units }:
+  outputs = { self, nixpkgs, flake-utils, machine-units, argument-search }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        # Get the outputs of the argument_search and machine_units flakes
-        argument_search_outputs = argument_search.outputs { self = argument_search; nixpkgs = nixpkgs; flake-utils = flake-utils; };
-        machine_units_outputs = machine_units.outputs { self = machine_units; nixpkgs = nixpkgs; flake-utils = flake-utils; };
       in {
         packages = {
-          # Expose packages for both projects
-          argument_search = argument_search_outputs.packages.${system}.default;
-          machine_units-cpp = machine_units_outputs.packages.${system}.machine_units-cpp;
-          machine_units-py = machine_units_outputs.packages.${system}.machine_units-py;
+          machine_units-cpp = machine-units.packages.${system}.machine_units-cpp;
+          machine_units-py = machine-units.packages.${system}.machine_units-py;
+          argument_search = argument-search.packages.${system}.argument_search;
+          default = machine-units.packages.${system}.default;
         };
 
         apps = {
-          # Expose apps for both projects
-          argument_search = argument_search_outputs.apps.${system}.default;
-          machine_units-cpp = machine_units_outputs.apps.${system}.machine_units-cpp;
-          machine_units-py = machine_units_outputs.apps.${system}.machine_units-py;
+          machine_units-cpp = machine-units.apps.${system}.machine_units-cpp;
+          machine_units-py = machine-units.apps.${system}.machine_units-py;
+          argument_search = argument-search.apps.${system}.argument_search;
+          default = machine-units.apps.${system}.default;
         };
 
         devShells = {
-          # Expose devShells for both projects
-          argument_search = argument_search_outputs.devShells.${system}.default;
-          machine_units = machine_units_outputs.devShells.${system}.default;
+          machine_units = machine-units.devShells.${system}.default;
+          argument_search = argument-search.devShells.${system}.default;
+          default = machine-units.devShells.${system}.default;
         };
-      });
+      }
+    );
 }
