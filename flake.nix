adb3711485d37dc2bdd8908d048dae035f80fdfa
seajoyer <imgarison@gmail.com>
2024-09-18 23:40:16 +0300

    flake

--- flake.nix
+++ flake.nix
@@ -4,54 +4,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-
-    machine-units-local.url = "path:./Machine_units";
-    machine-units-remote = {
+    machine-units = {
       url = "github:seajoyer/SupCo?dir=Machine_units";
-      flake = false;
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    machine-units.follows = "machine-units-local";
-
-    argument-search-local.url = "path:./Argument_search";
-    argument-search-remote = {
+    argument-search = {
       url = "github:seajoyer/SupCo?dir=Argument_search";
-      flake = false;
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    argument-search.follows = "argument-search-local";
   };
 
-  outputs = { self, nixpkgs, flake-utils, machine-units, argument-search, ... }:
+  outputs = { self, nixpkgs, flake-utils, machine-units, argument-search }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        # Function to import a flake from a path or fallback to remote
-        importFlake = localFlake: remoteFlake:
-          if builtins.pathExists (toString localFlake)
-          then localFlake
-          else import remoteFlake;
-
-        machine-units-flake = importFlake machine-units self.inputs.machine-units-remote;
-        argument-search-flake = importFlake argument-search self.inputs.argument-search-remote;
       in {
         packages = {
-          machine_units-cpp = machine-units-flake.packages.${system}.machine_units-cpp;
-          machine_units-py = machine-units-flake.packages.${system}.machine_units-py;
-          argument_search = argument-search-flake.packages.${system}.argument_search;
-          default = machine-units-flake.packages.${system}.default;
+          machine_units-cpp = machine-units.packages.${system}.machine_units-cpp;
+          machine_units-py = machine-units.packages.${system}.machine_units-py;
+          argument_search = argument-search.packages.${system}.argument_search;
+          default = machine-units.packages.${system}.default;
         };
 
         apps = {
-          machine_units-cpp = machine-units-flake.apps.${system}.machine_units-cpp;
-          machine_units-py = machine-units-flake.apps.${system}.machine_units-py;
-          argument_search = argument-search-flake.apps.${system}.argument_search;
-          default = machine-units-flake.apps.${system}.default or machine-units-flake.apps.${system}.machine_units-cpp;
+          machine_units-cpp = machine-units.apps.${system}.machine_units-cpp;
+          machine_units-py = machine-units.apps.${system}.machine_units-py;
+          argument_search = argument-search.apps.${system}.argument_search;
+          default = machine-units.apps.${system}.default or machine-units.apps.${system}.machine_units-cpp;
         };
 
         devShells = {
-          machine_units = machine-units-flake.devShells.${system}.default;
-          argument_search = argument-search-flake.devShells.${system}.default;
-          default = machine-units-flake.devShells.${system}.default;
+          machine_units = machine-units.devShells.${system}.default;
+          argument_search = argument-search.devShells.${system}.default;
+          default = machine-units.devShells.${system}.default;
         };
       }
     );
