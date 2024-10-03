0aa523dd4783b9d5469a7b534d5882d67c0aff77
seajoyer <imgarison@gmail.com>
2024-10-03 23:57:26 +0300

    Allocation functions

--- Dynamic_matrix/include/dynamic_matrix.h
+++ Dynamic_matrix/include/dynamic_matrix.h
@@ -1,43 +1,22 @@
 #pragma once
 
 #include "vector3d_structure.h"
+#include <cstddef>
 
-class DynamicMatrix {
+class DynamicMatrix{
 private:
     Vector3D** matrix;
+
     size_t rows;
     size_t cols;
 
-    void allocateMemory();
+    void   allocateMemory();
     void deallocateMemory();
 
 public:
     DynamicMatrix(size_t rows, size_t cols);
     ~DynamicMatrix();
 
-    // Rule of Three (since we're managing raw pointers)
-    DynamicMatrix(const DynamicMatrix& other);
-    DynamicMatrix& operator=(const DynamicMatrix& other);
-
-    // Element access and modification
-    Vector3D& at(size_t row, size_t col);
-    const Vector3D& at(size_t row, size_t col) const;
-
-    // Matrix operations
-    void deleteRow(size_t row);
-    void deleteColumn(size_t col);
-    void addRow(const Vector3D* newRow);
-    void addColumn(const Vector3D* newColumn);
-    void insertSubmatrix(const DynamicMatrix& submatrix, size_t startRow, size_t startCol);
-
-    // Basic matrix operations
-    DynamicMatrix operator+(const DynamicMatrix& other) const;
-    DynamicMatrix operator-(const DynamicMatrix& other) const;
-    DynamicMatrix operator*(const DynamicMatrix& other) const;
-    DynamicMatrix operator*(double scalar) const;
-
-    // Utility functions
-    size_t getRows() const { return rows; }
-    size_t getCols() const { return cols; }
-    void print() const;
+    size_t getRows() { return rows; }
+    size_t getCols() { return cols; }
 };
